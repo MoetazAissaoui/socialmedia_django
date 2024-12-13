2966a7b208b40be93f7ab59c6f4d8ebbe52c2ede
Aissaoui Moetaz <84248599+MoetazAissaoui@users.noreply.github.com>
2024-12-13 16:25:15 +0100

    update git ignore and ignore deployment script

--- socialmedia/deployment/deploy_prod.sh
+++ socialmedia/deployment/deploy_prod.sh
@@ -6,7 +6,7 @@ echo $USER
 ssh -T ubuntu@34.239.105.141 <<EOF
   cd project/Socialbook
   git pull
-  cd sclone
+  cd socialmedia
   sudo docker-compose build
   sudo docker-compose up -d
   sudo service nginx restart
